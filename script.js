c90b86a98cfc9010bac8348168cd11b049a91a14
Guilherme <guilhermebauer16@gmail.com>
2023-05-07 19:03:16 -0300

    Adicionando o challenge one ainda sem portabilidade

---
+++ script.js
@@ -0,0 +1,75 @@
+const textArea = document.querySelector(".texto");
+const mensagem = document.querySelector('.mensagem');
+const textoMensagem = document.querySelector(".texto-semMensagem")
+
+// As "chaves" de criptografia que utilizaremos são:
+// A letra "e" é convertida para "enter"
+// A letra "i" é convertida para "imes"
+// A letra "a" é convertida para "ai"
+// A letra "o" é convertida para "ober"
+// A letra "u" é convertida para "ufat"
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensagem.value = textoEncriptado;
+    textArea.value = "";
+
+
+
+}
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e" , "enter"], ["i" , "imes"], ["a", "ai"], ["o", "ober"],["u","ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0 ; i < matrizCodigo.length; i++){
+
+        if (stringEncriptada.includes(matrizCodigo[i][0])){
+
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+            textoMensagem.src = "";
+            mensagem.style.backgroundImage = "none";
+
+        }
+
+    }
+
+    return stringEncriptada;
+}
+
+
+function btnDescriptar(){
+    const textoDescriptado = descriptar(textArea.value);
+    mensagem.value = textoDescriptado;
+    textArea.value = "";
+
+}
+
+
+function descriptar(stringDesencriptada){
+
+    let matrizCodigo = [["e" , "enter"], ["i" , "imes"], ["a", "ai"], ["o", "ober"],["u","ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for (i = 0 ; i < matrizCodigo.length ; i++){
+
+        if (stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+            textoMensagem.src = "";
+            mensagem.style.backgroundImage = "none";
+        }
+    }
+
+    return stringDesencriptada;
+
+
+}
+
+function copia(){
+    textArea.value = mensagem.value
+    mensagem.value = ""
+    
+
+
+}
+
